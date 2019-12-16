38324a0649ff4b9a779f55f5f18908ec66be518a
Noah Gambee <noah@gambee.dev>
2019-12-16 14:38:46 -0700

    Import puzzle class to test file

--- test/Puzzle-test.js
+++ test/Puzzle-test.js
@@ -1,8 +1,20 @@
 import chai from 'chai';
 const expect = chai.expect;
 
+import Puzzle from '../src/classes/Puzzle'
+
 describe('Puzzle', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+  let puzzle;
+
+  beforeEach(() => {
+    puzzle = new Puzzle();
   });
+
+  it('should have access to the Player class', function() {
+    expect(puzzle).to.be.an.instanceOf(Puzzle);
+  });
+
+  it('should have a displayPuzzle method', function() {
+    expect(puzzle.displayPuzzle).to.be.a('function');
+  })
 });
