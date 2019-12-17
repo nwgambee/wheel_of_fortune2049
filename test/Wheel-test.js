4c02eb81fcab5f86b170bc501f8c68a5be351de2
Noah Gambee <noah@gambee.dev>
2019-12-17 14:45:18 -0700

    Update Wheel-test property tests

--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -1,17 +1,28 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-import Wheel from '../src/classes/Wheel'
+import Wheel from '../src/classes/Wheel';
+
 describe('Wheel', function() {
+
   let wheel;
 
   beforeEach(() => {
-    wheel = new Wheel();
+    let wheelData = [900, "BANKRUPT", 2500, 600, 700, 600, 650, 500, 700, "BANKRUPT", 600, 550, 500, 600, "BANKRUPT", "LOSE A TURN", 700, 800, 500, 650, 500, 900];
+    wheel = new Wheel(wheelData);
   });
 
   it('should have access to the Wheel class', function() {
     expect(wheel).to.be.an.instanceOf(Wheel);
   });
+
+  describe('Wheel properties', function() {
+
+    it('should have an elements property', function() {
+      expect(wheel.elements).to.deep.equal([900, "BANKRUPT", 2500, 600, 700, 600, 650, 500, 700, "BANKRUPT", 600, 550, 500, 600, "BANKRUPT", "LOSE A TURN", 700, 800, 500, 650, 500, 900]);
+    })
+  })
+
   it('should have a runBonusWheel method', function() {
     expect(wheel.runBonusWheel).to.be.a('function');
   })
