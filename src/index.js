68eaffa80f108861947c38e213ec9b9e3b785aac
Virginia Ladd <virginialadd2@gmail.com>
2019-12-18 20:15:27 -0700

    Vanna enters stage right

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function showGameBoard(event) {
   titleLogo.classList.add('small-logo');
   vannaHost.classList.remove('hidden');
   vannaHost.classList.add('slide-in-fwd-right');
-  vannaHost.classList.add('slide-left');
+  // vannaHost.classList.add('slide-left');
 }
 
 const myRand = () => {
