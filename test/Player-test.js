e2cdd6413e529e614a0a65a1416a677792e4e8fe
Noah Gambee <noah@gambee.dev>
2020-01-08 07:41:11 -0700

    Add buyVowel test

--- test/Player-test.js
+++ test/Player-test.js
@@ -18,5 +18,13 @@ describe('Player', function() {
   it('should have a spinWheel method', function() {
     expect(player.spinWheel).to.be.a('function');
   })
+  it('should have a buyVowel method that updates the round score', function() {
+    player.buyVowel();
+    expect(player.roundScore).to.deep.equal(-100);
+  });
+  it('should have a calculateScore method that updates player.roundScore', function() {
+    player.calculateScore(8);
+    expect(player.roundScore).to.deep.equal(player.roundScore * 8);
+  }
 
 });
